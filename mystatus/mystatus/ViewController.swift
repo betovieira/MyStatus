6c416be9283161c61e0343611b020f97b2429cf9
Paulo Henrique Leite <paulo-http@live.com>
2015-05-04 18:16:08 -0300

    set and get singleton vars

--- mystatus/mystatus/ViewController.swift
+++ mystatus/mystatus/ViewController.swift
@@ -17,13 +17,38 @@ class ViewController: UIViewController {
         println(Singleton.sharedInstance.name);
         super.viewDidLoad()
         
-
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBAction func getButton1(sender: UIButton) {
+        Singleton.sharedInstance.buttonType = 1;
+        Singleton.sharedInstance.customText = "I'm developing";
+        Singleton.sharedInstance.name = nameTextField.text;
+    }
+    
+    @IBAction func getButton2(sender: UIButton) {
+        Singleton.sharedInstance.buttonType = 2;
+        Singleton.sharedInstance.customText = "I'm free";
+        Singleton.sharedInstance.name = nameTextField.text;
+    }
+    
+    @IBAction func getButton3(sender: UIButton) {
+        Singleton.sharedInstance.buttonType = 3;
+        Singleton.sharedInstance.customText = "I'm in bathroom";
+        Singleton.sharedInstance.name = nameTextField.text;
+    }
+    
+    @IBAction func getButton4(sender: UIButton) {
+        Singleton.sharedInstance.buttonType = 4;
+        Singleton.sharedInstance.customText = "Custom";
+        Singleton.sharedInstance.name = nameTextField.text;        
     }
 }
 
