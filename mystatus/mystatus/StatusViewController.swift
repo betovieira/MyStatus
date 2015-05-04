52db4f6583dda09ca69d2a8a656371d6a05c0cca
Humberto Vieira de Castro <humbertovieira.castro@gmail.com>
2015-05-04 18:13:37 -0300

    commit statusViewController

--- mystatus/mystatus/StatusViewController.swift
+++ mystatus/mystatus/StatusViewController.swift
@@ -9,9 +9,32 @@
 import UIKit
 
 class StatusViewController: UIViewController {
-
+    @IBOutlet var labelStatus: UILabel!
+    @IBOutlet var labelSignature: UILabel!
+    var typeButton = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        typeButton = Singleton.sharedInstance.buttonType
+        
+        labelStatus.text = Singleton.sharedInstance.customText
+        labelSignature.text = Singleton.sharedInstance.name
+        
+        if(typeButton == 1)
+        {
+            self.view.backgroundColor = .redColor()
+        }else if(typeButton == 2){
+           self.view.backgroundColor = .greenColor()
+            
+        }else if(typeButton == 3){
+            self.view.backgroundColor = .redColor()
+        }else if(typeButton == 4){
+            self.view.backgroundColor = .greenColor()
+        }else{
+            println("Error")
+        }
+        
         // Do any additional setup after loading the view.
     }
 
