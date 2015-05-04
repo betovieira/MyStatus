098ac46a5a911e3e03ff4c448f8f2f8c7dc9cc7e
Paulo Henrique Leite <paulo-http@live.com>
2015-05-04 15:38:13 -0300

    tela 1

---
+++ mystatus/mystatus/PassingVariable.swift
@@ -0,0 +1,9 @@
+//
+//  PassingVariable.swift
+//  mystatus
+//
+//  Created by Paulo Henrique Leite on 5/4/15.
+//  Copyright (c) 2015 Humberto Vieira de Castro. All rights reserved.
+//
+
+import Foundation
